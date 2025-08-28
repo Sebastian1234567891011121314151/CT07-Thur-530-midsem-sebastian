53a23cb361ee3d56fe66bc8c0b33083c58b84486
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-28 09:49:37 +0000

    test

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,15 @@
+function setup(){
+    new Canvas(600,400);
+    
+world.gravity.y=3;
+    
+}
 
+function draw(){
+
+background("white");
+if (mouse.presses()){
+ball = new Sprite (mouseX, mouseY,);
+ball.diameter= 50;
+}
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,21 @@
+let floor;
 
+
+
+
+function setup(){
+new Canvas(600,400);
+floor = new Sprite (300,390,600,30,"static");
+world.gravity.y=3;
+
+}
+
+function draw(){
+
+background("white");
+if (mouse.presses()){
+ball = new Sprite (mouseX, mouseY,);
+ball.diameter= 50;
+ball.bounciness=0.5;
+}
+}
